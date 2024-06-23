530820f03d8a27a8ad421f412b402f46d9cea538
theUniC <theunic@gmail.com>
2024-06-23 12:30:49 +0200

    Some fixes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     <SlimLayout>
       <div className="flex">
         <Link href="/" aria-label="Home">
-          <Logo className="h-10 w-auto" />
+          <Logo />
         </Link>
       </div>
       <h2 className="mt-20 text-lg font-semibold text-gray-900">
